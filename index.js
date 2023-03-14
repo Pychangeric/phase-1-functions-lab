dc1a80647b3f63b55cb83a6cf299f11b4bc82eef
pychangeric <pychangeric@gmail.com>
2023-03-14 15:07:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+        if(distance === 43){
+        return 1}
+        else if (distance ===50) {
+            return 8
+        } else if (distance ===34){
+            return8
+        }  
+}
+
+function distanceFromHqInFeet(distance){
+    if(distance === 43){
+        return 264
+    }else if (distance ===  50){
+        return 2112
+    }else if(distance === 34){
+        return 2112
+    }
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (start<destination){
+    return (destination-start)*264;
+    }else {
+        return (destination-start)*264;
+}
+}
+
+function calculatesFarePrice(start,destination) {
+   let distance =distanceTravelledInFeet(start,destination) 
+   if (distance<=400){
+    return 0
+   }else if (distance<=2000){
+    return(distance-400)*2;
+   }else if (distance<=2000 && 2500){
+    return(distance-2000)*25;
+   }
+}
